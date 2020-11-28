b89d875169e6fa1cd9b19a961cf193a5c395be4f
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-11-28 15:00:39 +0200

    set active tab

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import { SidebarStyles } from './styled'
 import { prefetchPosts } from '../hooks'
 
 export default function Sidebar() {
+  const location = useLocation()
   return (
     <SidebarStyles>
+      {console.log(location)}
       <ListStyles>
         <li>
           <Link to="/">Home</Link>
         </li>
+        {location.pathname === '/' && <hr />}
         <li onMouseEnter={() => prefetchPosts()}>
           <Link to="/blog">Blog</Link>
         </li>
-        <hr />
+        {location.pathname === '/blog' && <hr />}
         <li onMouseEnter={() => prefetchPosts()}>
           <Link to="/admin">Admin</Link>
         </li>
+        {location.pathname === '/admin' && <hr />}
       </ListStyles>
     </SidebarStyles>
   )
